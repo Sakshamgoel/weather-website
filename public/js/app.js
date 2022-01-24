220d1d3f635dac1ac96960116f5c48dcc8e6bf9e
Saksham Goel <szg0112@auburn.edu>
2022-01-24 15:14:19 -0600

    fixed localhost bug

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,7 @@ weatherForm.addEventListener('submit', (e) => {
     
     console.log(location);
 
-    const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
 
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
